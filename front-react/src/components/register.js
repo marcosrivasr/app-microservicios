d5fe61932a6bb21ae91b9539e6b5daf37e40c083
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-03-28 15:59:23 -0700

    url change

--- front-react/src/components/register.js
+++ front-react/src/components/register.js
@@ -10,7 +10,7 @@ export default function Register() {
     const handleSubmit = async e => {
         e.preventDefault();
 
-        const data = await fetch('http://localhost:3001/register', {
+        const data = await fetch('http://localhost:5555/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
